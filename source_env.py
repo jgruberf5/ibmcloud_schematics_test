2f9caad4bc0669437e835d56f0b3b35bef6960bd
John Gruber <jgruber@f5.com>
2020-12-15 09:13:38 -0600

    added python libs in path

--- source_env.py
+++ source_env.py
@@ -4,14 +4,23 @@ import sys
 import os
 import json
 
+
+def list_of_packages(startpath):
+    for root, dirs, files in os.walk(startpath):
+
+
+
 def main():
     # jsondata = json.loads(sys.stdin.read())
     jsondata = {}
     env = os.environ
     for v in env.keys():
         jsondata[v] = env[v]
-    for key in sys.modules.keys():
-        jsondata[key] = 'installed'
+    for pathname in sys.path:
+        file_index = 0
+        for root,dirs,files in os.walk(pathname):
+            jsondata["pythonpath_%d" % file_index] = root
+            file_index = file_index + 1
     sys.stdout.write(json.dumps(jsondata))
 
 if __name__ == '__main__':
