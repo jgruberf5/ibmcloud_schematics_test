c72c3b7b2466fb1683d245569e40da50a6b61a3c
John Gruber <jgruber@f5.com>
2020-12-15 11:13:41 -0600

    added python libs in path

--- source_env.py
+++ source_env.py
@@ -3,25 +3,28 @@
 import sys
 import os
 import json
-import pip
+import subprocess
 
 def install_package(package_name):
-    pip.main(['install', package_name])
+    proc = subprocess.Popen(['pip', 'install', package_name], stdout=subprocess.PIPE)
+    return proc.stdout
 
 def main():
-    install_package('wheel')
-    install_package('pyyaml')
-    # jsondata = json.loads(sys.stdin.read())
     jsondata = {}
+    std_out = install_package('wheel')
+    jsondata['installwheelout'] = std_out.read().replace('\n','|')
+    std_out = install_package('pyyaml')
+    jsondata['installpyyaml'] = std_out.read().replace('\n','|')
+    # jsondata = json.loads(sys.stdin.read())
     env = os.environ
     for v in env.keys():
         jsondata[v] = env[v]
-    for pathname in sys.path:
-        file_index = 0
-        for root,dirs,files in os.walk(pathname):
-            if 'ansible' not in root:
-                jsondata["pythonpath_%d" % file_index] = root
-                file_index = file_index + 1
+    #for pathname in sys.path:
+    #    file_index = 0
+    #    for root,dirs,files in os.walk(pathname):
+    #        if 'ansible' not in root:
+    #            jsondata["pythonpath_%d" % file_index] = root
+    #            file_index = file_index + 1
     sys.stdout.write(json.dumps(jsondata))
 
 if __name__ == '__main__':
