634a29732d5a0013eb21988a52bb3d61740d0bd6
John Gruber <j.gruber@f5.com>
2020-12-15 21:48:41 -0600

    fixed output joining

--- source_env.py
+++ source_env.py
@@ -23,12 +23,12 @@ def main():
     env = os.environ
     for v in env.keys():
         jsondata[v] = env[v]
-    #for pathname in sys.path:
-    #    file_index = 0
-    #    for root,dirs,files in os.walk(pathname):
-    #        if 'ansible' not in root:
-    #            jsondata["pythonpath_%d" % file_index] = root
-    #            file_index = file_index + 1
+    for pathname in sys.path:
+        file_index = 0
+        for root,dirs,files in os.walk(pathname):
+            if 'ansible' not in root:
+                jsondata["pythonpath_%d" % file_index] = root
+                file_index = file_index + 1
     sys.stdout.write(json.dumps(jsondata))
 
 if __name__ == '__main__':
